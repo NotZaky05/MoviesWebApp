17d7ed35c988048b5e2126b3b613100671b74d7a
Zaky <109673537+NotZaky05@users.noreply.github.com>
2025-05-22 21:31:36 +0700

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -86,7 +86,7 @@ const MovieDetails = () => {
   return (
     <div className="movie-details-container">
       <button className="back-button" onClick={handleGoBack}>
-        < Back
+         Back
       </button>
 
       <div className="movie-details">
